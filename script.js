37b6ee8b6308d94618a1647827374da5ffb8b26e
sachinmch23 <sachinmch638@gmail.com>
2025-12-01 12:57:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+const openBtn = document.getElementById("openBtn");
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("closeBtn");
+
+openBtn.addEventListener("click", () =>{
+    popup.style.display ="block";
+   
+})
+
+closeBtn.addEventListener("click" , () => {
+    popup.style.display="none";
+
+})
+
+
+
+
+
+
+
+
+
+
+// ////////////////////////////////////////////  fire base cdn /////////////////////////////////////
+
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQl55bPrJmIl_kkJE8wZyl8Tbj33D3mns",
+  authDomain: "portfolio-connect.firebaseapp.com",
+  projectId: "portfolio-connect",
+  storageBucket: "portfolio-connect.appspot.com",
+  messagingSenderId: "985114298964",
+  appId: "1:985114298964:web:9fd62d0a1aa268c5c8fbc1",
+  measurementId: "G-PL3SC16J4F",
+  databaseURL: "https://portfoliocontect-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const db = getDatabase(app);
+const db = getDatabase(app)
+
+//console.log("Firebase Connected!");
+
+set(ref(db, "test"), {
+  name: "Sachin",
+  mobile: "1234567890",
+  email: "sachin@gmail.com"
+})
+  .then(() => console.log("Data saved!"))
+  .catch((err) => console.log("Error:", err));
+
+// CONTACT FORM
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const mobile = document.getElementById("mobile").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("msg").value;
+
+  set(ref(db, "contacts/" + Date.now()), {
+    name,
+    mobile,
+    email,
+    message,
+  })
+    .then(() => {
+      alert("Form submitted successfully!");
+      form.reset();
+    })
+    .catch((err) => {
+      alert("Error");
+      console.log(err);
+    });
+});
